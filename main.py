465c340cb83a4f6893b59fca3c45edbd958cca19
Ryan Goldie <rgoldie@cub.uca.edu>
2025-02-11 17:44:51 -0600

    added basic file reader and time measure

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-import time
+import time 
+from bubblesort import bubble_sort
+
+with open('100.txt') as f:
+    number_list = []
+    for line in f:
+        line = line.split() # to deal with blank 
+        if line:            # lines (ie skip them)
+            line = [int(i) for i in line]
+            number_list.append(line)
+
+start = time.time()
+bubble_sort(number_list)
+stop = time.time()
+print(stop-start)
