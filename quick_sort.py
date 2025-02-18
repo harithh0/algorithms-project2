b8e9d1de6f26c8d2d1ef2836bb7b4566337f5358
harithh0 <169714068+harithh0@users.noreply.github.com>
2025-02-18 17:35:54 -0600

    reverted quicksort

--- quick_sort.py
+++ quick_sort.py
@@ -8,8 +8,8 @@ def quicksort(arr: list[int]):
         less_than: list[int] = []
         greater_than: list[int] = []
         same_value: list[int] = []
-        pivot: int = arr[0]  # -- bad - causes imbalance
-        # pivot: int = arr[random.randint(0, len(arr) - 1)]  # much better
+        # pivot: int = arr[0]  # -- bad - causes imbalance
+        pivot: int = arr[random.randint(0, len(arr) - 1)]  # much better
         for i in arr:
             if i < pivot:
                 less_than.append(i)
